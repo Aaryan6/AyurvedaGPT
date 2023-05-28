5433e8a205e43fa87aec8bf58c8abdbe3f4d04ba
Aaryan Patel <aaryanpatel683@gmail.com>
2023-05-28 13:59:17 +0530

    changed sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,22 +2,16 @@ const Sidebar = () => {
   return (
     <div className="max-w-[24rem] hidden lg:block">
       <div className="mx-auto mt-10 px-4">
+        <div className="bg-[#ECF1F5] dark:bg-[#171925] dark:text-slate-300 px-6 py-6 rounded-xl text-center  my-5">
+          <h3 className="text-lg font-medium">What is Ayurveda?</h3>
+          <p className="mt-2 text-sm leading-normal">
+            Ayurveda is an ancient system of medicine & philosophy originating
+            in India. It is based on the belief that health is a balance of
+            three doshas, or energies: Vata, Pitta, and Kapha.
+          </p>
+        </div>
         <div className="bg-[#ECF1F5] dark:bg-[#171925] dark:text-slate-300 px-6 py-6 rounded-xl text-center my-5">
-          <h3 className="text-lg font-medium">About it</h3>
-          <p className="mt-2 text-sm">
-            Ayurveda GPT is a chat model that answers your questions based on
-            ayurveda knowledge.
-          </p>
-        </div>
-        <div className="bg-[#ECF1F5] dark:bg-[#171925] dark:text-slate-300 px-6 py-6 rounded-xl text-center  my-5">
-          <h3 className="text-lg font-medium">How it works?</h3>
-          <p className="mt-2 text-sm">
-            Ayurveda GPT is a chat model that answers your questions based on
-            ayurveda knowledge.
-          </p>
-        </div>
-        <div className="bg-[#ECF1F5] dark:bg-[#171925] dark:text-slate-300 px-6 py-6 rounded-xl text-center  my-5">
-          <h3 className="text-lg font-medium">Limitations</h3>
+          <h3 className="text-lg font-medium">What it does?</h3>
           <p className="mt-2 text-sm">
             Ayurveda GPT is a chat model that answers your questions based on
             ayurveda knowledge.

9178da22eddca4b0bd92a0f5a6532f9e899194e4
Aaryan Patel <aaryanpatel683@gmail.com>
2023-05-31 19:48:59 +0530

    add metadata and vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <main className="bg-[#ecf1f5] dark:bg-[#323b4d] h-screen overflow-scroll flex flex-col">
+    <main className="bg-[#ddeef2] dark:bg-[#323b4d] h-screen overflow-scroll flex flex-col">
       <Navbar />
       <div className="flex flex-1 w-full max-w-[90rem] sm:mx-auto">
         <Sidebar />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,8 +26,8 @@ const Navbar = () => {
     <nav className="flex items-center justify-between p-4 sm:p-6 px-4 sm:px-10 max-w-8xl mx-auto w-full">
       <div className="flex items-center">
         <div className="cursor-pointer flex items-center">
-          <img src="/logo.png" alt="" className="w-9 mr-2" />
-          <span className="logo text-black dark:text-[#dfdfdf] font-semibold text-base sm:text-xl">
+          <img src="/favicon.png" alt="" className="w-9 mr-2" />
+          <span className="logo text-black dark:text-teal-100 font-semibold text-base sm:text-xl">
             AyurvedaGPT
           </span>
         </div>
@@ -37,18 +37,15 @@ const Navbar = () => {
           href="https://github.com/Aaryan6/AyurvedaGPT-react"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-white hover:text-slate-200 mr-6"
+          className="text-white mr-6"
         >
-          <FaGithub className="text-xl sm:text-2xl text-black dark:text-[#dfdfdf]" />
+          <FaGithub className="text-xl sm:text-2xl text-black dark:text-[#fff]" />
         </a>
-        <button
-          className="text-white hover:text-gray-300"
-          onClick={toggleDarkMode}
-        >
+        <button className="text-white" onClick={toggleDarkMode}>
           {isDarkMode ? (
-            <FaSun className="text-xl sm:text-2xl  text-black dark:text-[#dfdfdf]" />
+            <FaSun className="text-xl sm:text-2xl  text-black dark:text-[#fff]" />
           ) : (
-            <FaMoon className="text-xl sm:text-2xl  text-black dark:text-[#dfdfdf]" />
+            <FaMoon className="text-xl sm:text-2xl  text-black dark:text-[#fff]" />
           )}
         </button>
       </div>
